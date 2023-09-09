5dbad73013b19ee5d6124e23af81b1a5f2cdbfbf
Pablo P Varela <pablo@pablopunk.com>
2023-09-09 19:19:52 +0200

    smaller window

--- lua/todo.lua
+++ lua/todo.lua
@@ -29,8 +29,8 @@ local close_window_vim_cmd = "lua require('todo').close_todo_window()"
 function TodoWindow:create_window()
   local width = vim.api.nvim_get_option "columns"
   local height = vim.api.nvim_get_option "lines"
-  local win_height = math.ceil(height * 0.8 - 4)
-  local win_width = math.ceil(width * 0.8)
+  local win_height = math.ceil(height * 0.5 - 4)
+  local win_width = math.ceil(width * 0.5)
   local row = math.ceil((height - win_height) / 2 - 1)
   local col = math.ceil((width - win_width) / 2)
   local opts = {
